4c580558d594dcc09ea34c936ce0a84bb14d04f4
mohamed <elkahzen1@gmail.com>
2021-08-09 20:10:10 -0500

    first commit

---
+++ src/main/java/org/launchcode/Secretrestaurantreview/SecretRestaurantReviewApplication.java
@@ -0,0 +1,13 @@
+package org.launchcode.Secretrestaurantreview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretRestaurantReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecretRestaurantReviewApplication.class, args);
+	}
+
+}
